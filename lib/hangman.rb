9245638cf24e29813e84986efa48c994077eec7e
Matt Turner <mattturner1911@gmail.com>
2023-02-20 07:33:11 -0500

    Created the Game class. Game has #start which will start the game, and #random_word is now inside Game and assigns random word to @word instance variable.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,30 @@
-def random_word
-  txt = 'google-10000-english-no-swears.txt'
+class Game
+  attr_reader :word, :key
 
-  list = []
-  file = File.open(txt)
-  until file.eof?
-    line = file.readline.strip
-    if line.length >= 5 && line.length <= 12
-      list.push(line)
-    end
+  @word = ''
+  @key = ''
+
+  def start
+    puts 'Would you like to play hangman?'
+    puts 'Press 1 to "Start" or 2 to "Load Game"'
+    gets.chomp == '1' ? random_word : 'Thank you for playing.'
   end
 
-  list.sample
+  def random_word
+    # holds google text file
+    txt = 'google-10000-english-no-swears.txt'
+    # empty array to hold words of a 5-12 char length
+    list = []
+    # opens file and pushes the word to list array
+    file = File.open(txt)
+    until file.eof?
+      line = file.readline.strip
+      line.length >= 5 && line.length <= 12 ? list.push(line) : nil
+    end
+    # returns a randomly selected word from list
+    @word = list.sample
+  end
 end
 
-puts random_word
+hangman = Game.new
+hangman.start

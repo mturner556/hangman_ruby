5577ebdbc97b06df8955ada721c74cfb8dd77df3
Matt Turner <mattturner1911@gmail.com>
2023-02-21 07:02:57 -0500

    Added several methods to the Game class w/ comments to work on. Removed comments inside of #random_word, and wrote a general comment above it. Added @guesses array to store what the player has guessed.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,8 +1,9 @@
 class Game
-  attr_reader :word, :key
+  attr_reader :word, :key, :guesses
 
   @word = ''
   @key = ''
+  @guesses = []
 
   def start
     puts 'Would you like to play hangman?'
@@ -10,19 +11,42 @@ class Game
     gets.chomp == '1' ? random_word : 'Thank you for playing.'
   end
 
+  # selects a random word and assigns it to the @word variable
   def random_word
-    # holds google text file
     txt = 'google-10000-english-no-swears.txt'
-    # empty array to hold words of a 5-12 char length
     list = []
-    # opens file and pushes the word to list array
     file = File.open(txt)
     until file.eof?
       line = file.readline.strip
       line.length >= 5 && line.length <= 12 ? list.push(line) : nil
     end
-    # returns a randomly selected word from list
+
     @word = list.sample
+  end
+
+  # will save the state of the game
+  def save_game
+
+  end
+
+  # loads a saved game
+  def load_game
+
+  end
+
+  # will hide the random word
+  def hide_word
+
+  end
+
+  # will store the guesses the player made
+  def store_guess
+
+  end
+
+  # checks the player's guess
+  def check_guess
+
   end
 end
 

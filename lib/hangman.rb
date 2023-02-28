395716f725e1acee1152ab1e56aa2ecf4cc41c4d
Matt Turner <mattturner1911@gmail.com>
2023-02-28 08:16:06 -0500

    #turn method calls the #random_word, and handles the turn order for the player's input. #player_input gets the player's input, checks if the input is valid, and assigns it the proper class variables. #store_guess is running into a nomethod error for pushing the inputs into the @guesses array.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :word, :key, :guesses
+  attr_accessor :word, :key, :guesses
 
   @word = ''
   @key = ''
@@ -9,7 +9,7 @@ class Game
   def start
     puts 'Would you like to play hangman?'
     puts 'Press 1 to "Start" or 2 to "Load Game"'
-    gets.chomp == '1' ? random_word : 'Thank you for playing.'
+    gets.chomp == '1' ? turn : load_game
   end
 
   # selects a random word and assigns it to the @word variable
@@ -27,25 +27,35 @@ class Game
 
   # gets the player's input
   def player_input
-    @player_guess = gets.chomp
+    player_guess = gets.chomp
+
+    if player_guess.length == 1
+      @player_guess = player_guess
+      store_guess(@player_guess)
+    elsif player_guess == 'save'
+      save_game
+    else
+      puts 'Input ERROR'
+    end
   end
 
   # plays rounds of the game until the guesses match the @word
   def turn
+    random_word
     until @player_key == @word
       display
       player_input
-
+    end
   end
 
   # will save the state of the game
   def save_game
-
+    puts "Save ERROR"
   end
 
   # loads a saved game
   def load_game
-
+    puts 'Load ERROR'
   end
 
   # will mask the random word from the player
@@ -55,8 +65,8 @@ class Game
   end
 
   # will store the guesses the player made
-  def store_guess
-
+  def store_guess(guess)
+    @guesses.push(guess)
   end
 
   # checks the player's guess
@@ -65,6 +75,7 @@ class Game
   end
 
   def display
+    puts "Save game by pressing 'save'"
     puts "#{mask}"
     puts "__________"
     puts "#{guesses}"
@@ -73,4 +84,3 @@ end
 
 hangman = Game.new
 hangman.start
-hangman.display

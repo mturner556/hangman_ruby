abd13900994a07d91a307bdf615518a52cc67b68
Matt Turner <mattturner1911@gmail.com>
2023-06-27 08:01:21 -0400

    The loading module now loads a single saved file.

--- lib/file_loading.rb
+++ lib/file_loading.rb
@@ -1,33 +1,21 @@
 module FileLoading
   def load
-    puts Dir.chdir('./saved_games')
-    puts Dir.pwd
-    # if !Dir.exist?('./saved_games')
-    #   loop do
-    #     puts 'No saved games.'
-    #     break if user_input == '1'
-    #   end
-    # else
-    #   loaded_game = File.open("/saved_games/#{loaded_file_name}.yml", 'r') do |file|
-    #     YAML.load(file)
-    #   end
-    #   loaded_game.play
-    # end
+    file_name = loaded_file_name
+    File.open("./saved_games/#{file_name}.yml") do |f|
+      game = YAML::load(f, permitted_classes: [Hangman])
+      game.play
+    end
   end
 
   def loaded_file_name
-    saved_games = Dir.glob('saved_game/').map { |game| game.split('/').last.split('.yml').first }
-    file_count = saved_games.size
-    puts file_count
-    # loop do
-    #   puts 'Enter a number of a game to load.'
-    #   saved_games.each_with_index { |game, i| puts "#{i + 1}: #{game}" }
-    #   file_num = gets.chomp.strip
-    #   if /\A[1-9]+\Z/.match?(file_num) && file_num.to_i <= file_count
-    #     return saved_games[file_num.to_i - 1]
-    #   end
-    #   puts 'Invalid input.'
-    # end
+    puts 'Please enter the name of the file you would like to play.'
+    saved_games
+    gets.chomp.downcase.strip
+  end
+
+  def saved_games
+    puts 'Saved games:'
+    puts Dir['./saved_games/*'].map { |f| f.split('/')[-1].split('.')[0] }
   end
 end
 
